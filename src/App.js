869596f20e432e1c0ed3468b20244643b377c316
David Kim <cjoak1028@gmail.com>
2022-10-28 16:59:01 -0400

    added variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
