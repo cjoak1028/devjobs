ff0ea9646ce628a582fe73e6b110f284d7003bf9
David Kim <cjoak1028@gmail.com>
2022-11-02 13:14:29 -0400

    Added icons to theme switch

--- src/components/Header/ThemeSwitch.js
+++ src/components/Header/ThemeSwitch.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./ThemeSwitch.module.css";
+import iconSun from "../../assets/desktop/icon-sun.svg";
+import iconMoon from "../../assets/desktop/icon-moon.svg";
 
 const ThemeSwitch = () => {
   return (
-    <label className={styles.themeSwitch}>
-      <input type="checkbox" />
-      <span class={styles.slider}></span>
-    </label>
+    <div className={styles.switchContainer}>
+      <img src={iconSun} alt="light theme icon" />
+      <label className={styles.themeSwitch}>
+        <input type="checkbox" />
+        <span class={styles.slider}></span>
+      </label>
+      <img src={iconMoon} alt="light theme icon" />
+    </div>
   );
 };
 
