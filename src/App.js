796ba2afbaaab47062061ba44973ed5c8a4e0a07
David Kim <cjoak1028@gmail.com>
2022-11-03 12:11:09 -0400

    Created JobsList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
 import Header from "./components/Header/Header";
+import JobsList from "./components/JobsList/JobsList";
 
 function App() {
   return (
     <div className={"App"}>
       <Header />
+      <JobsList />
     </div>
   );
 }
